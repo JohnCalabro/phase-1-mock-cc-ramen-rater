de609f8bc622f3c8a6ac0f9cdcd885460c3941d3
johncalabro <j.calabro4@gmail.com>
2022-04-13 12:07:11 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // write your code here
+/*
+1. See all ramen images in the div with the id of ramen-menu. 
+When the page loads, request the data from the server to get all the 
+ramen objects. Then, display the image for each of the ramen using an 
+img tag inside the #ramen-menu div.
+2. Click on an image from the #ramen-menu div and see all the info 
+about that ramen displayed inside the #ramen-detail div and where it 
+says insert comment here and insert rating here.
+3. Create a new ramen after submitting the new-ramen form. 
+The new ramen should be added to the#ramen-menu div.
+ The new ramen does not need to persist; in other words, if you 
+ refresh the page, it's okay that the new ramen is no longer on the 
+ page.
+*/
+//fetch('http://localhost:3000/ramens/:id');
+    //.then(resp) =>
+
+
+   
+    http://localhost:3000GET/ramensGET/ramens/:id
+
+
+    //what next?
+//1. figure out why I cannot connect to local API
+ //2. review every relevant lab, drill those concepts into my
+ //brain. Likely dog CEO, and tail labs
+ //3. Take advantage of the code challenge reviews
